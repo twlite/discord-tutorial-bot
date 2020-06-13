6fbb130b2fcece8f911c3430dd52724996b6daea
Snowflake <46562212+Snowflake107@users.noreply.github.com>
2020-06-13 20:17:57 +0545

    Create leaderboard.js

---
+++ commands/leaderboard.js
@@ -0,0 +1,38 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+    let data = client.db.all().filter(i => i.ID.startsWith("xp_")).sort((a, b) => b.data - a.data);
+    if (data.length < 1) return message.channel.send("No leaderboard");
+    let myrank = data.find(u => u.ID == `xp_${message.author.id}`) || "N/A";
+    data.length = 20;
+    let lb = [];
+    data.forEach(async i => {
+        let id = i.ID.split("_")[1];
+        let user = await client.users.fetch(id);
+        user = user ? user.tag : "Unknown User#0000";
+        let rank = data.indexOf(i) + 1;
+        let level = client.db.get(`level_${id}`);
+        let xp = i.data;
+        let xpreq = Math.floor(Math.pow(level / 0.1, 2));
+        lb.push({
+            user: { id, tag: user },
+            rank,
+            level,
+            xp,
+            xpreq
+        });
+    });
+
+    const embed = new MessageEmbed()
+    .setTitle("Leaderboard")
+    .setColor("RANDOM")
+    lb.forEach(d => {
+        embed.addField(`${d.rank}. ${d.user.tag}`, `**Level** - ${d.level}\n**XP** - ${d.xp} / ${d.xpreq}`);
+    });
+    embed.setFooter(`Your Position: ${myrank}`);
+    return message.channel.send(embed);
+};
+
+module.exports.help = {
+    name: "leaderboard"
+};
