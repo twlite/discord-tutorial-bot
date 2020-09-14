c3f8a84d2d714ab628a0b95aecaae648dab4dd71
Snowflake <dhunganakunjan1@gmail.com>
2020-09-14 16:45:30 +0545

    Add files via upload

---
+++ commands/discrim.js
@@ -0,0 +1,21 @@
+const hastebin = require("hastebin-gen");
+
+module.exports.run = (client, message, args) => {
+    const query = args[0];
+    if (!query) return message.channel.send("Please include a discriminator!");
+
+    const users = client.users.cache.filter(user => user.discriminator === query).map(m => m.tag);
+    if (!users.length) return message.channel.send(`No users found with discriminator **#${query}**!`);
+
+    hastebin(users.join("\n"))
+        .then(haste => {
+            message.channel.send(`${users.length + 1} Users found with discriminator **#${query}**!\n${haste}`);
+        })
+        .catch(e => {
+            message.channel.send("Something went wrong, please try again later!");
+        });
+}
+
+module.exports.help = {
+    name: "discrim"
+};
