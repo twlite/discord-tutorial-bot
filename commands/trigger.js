adf39ee581c5a99449095fae1bb2a21b710b3322
Snowflake <devsnowflake107@gmail.com>
2020-10-04 12:34:57 +0545

    use v5 triggered

--- commands/trigger.js
+++ commands/trigger.js
@@ -1,10 +1,9 @@
-const Canvacord = require("canvacord");
-const canvacord = new Canvacord();
+const canvacord = require("canvacord");
 const { MessageAttachment } = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
     let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
-    let triggered = await canvacord.trigger(user.displayAvatarURL({ format: "png", dynamic: false }));
+    let triggered = await canvacord.Canvas.trigger(user.displayAvatarURL({ format: "png", dynamic: false }));
     let attachment = new MessageAttachment(triggered, "triggered.gif");
     return message.channel.send(attachment);
 }
