cd08b805d077c625493f064b8ede3d4c30b98d79
Snowflake <devsnowflake107@gmail.com>
2020-10-04 12:34:09 +0545

    v5 rank card

--- commands/rank.js
+++ commands/rank.js
@@ -1,4 +1,5 @@
 const { MessageAttachment } = require("discord.js");
+const canvacord = require("canvacord");
 
 module.exports.run = async (client, message, args) => {
   let user =
@@ -8,7 +9,6 @@ module.exports.run = async (client, message, args) => {
     message.author;
 
   let level = client.db.get(`level_${user.id}`) || 0;
-  level = level.toString();
   let exp = client.db.get(`xp_${user.id}`) || 0;
   let neededXP = Math.floor(Math.pow(level / 0.1, 2));
 
@@ -17,23 +17,36 @@ module.exports.run = async (client, message, args) => {
     .filter(i => i.ID.startsWith("xp_"))
     .sort((a, b) => b.data - a.data);
   let rank = every.map(x => x.ID).indexOf(`xp_${user.id}`) + 1;
-  rank = rank.toString();
-  let img = await client.canvas.rank({
-    username: user.username,
-    discrim: user.discriminator,
-    currentXP: exp.toString(),
-    neededXP: neededXP.toString(),
-    rank,
-    level,
-    avatarURL: user.displayAvatarURL({ format: "png" }),
-    background: "https://images.unsplash.com/photo-1523821741446-edb2b68bb7a0?ixlib=rb-1.2.1&w=1000&q=80"
-  });
+
+//   let img = await canvacord.rank({
+//     username: user.username,
+//     discrim: user.discriminator,
+//     currentXP: exp.toString(),
+//     neededXP: neededXP.toString(),
+//     rank,
+//     level,
+//     avatarURL: user.displayAvatarURL({ format: "png" }),
+//     background: "https://images.unsplash.com/photo-1523821741446-edb2b68bb7a0?ixlib=rb-1.2.1&w=1000&q=80"
+//   });
+  
+  const card = new canvacord.Rank()
+    .setUsername(user.username)
+    .setDiscriminator(user.discriminator)
+    .setRank(rank)
+    .setLevel(level)
+    .setCurrentXP(exp)
+    .setRequiredXP(neededXP)
+    .setStatus(user.presence.status)
+    .setAvatar(user.displayAvatarURL({ format: "png", size: 1024 }));
+
+  const img = await card.build();
+  
   return message.channel.send(new MessageAttachment(img, "rank.png"));
 };
 
 function match(msg, i) {
-  if (!msg) return undefined;
-  if (!i) return undefined;
+  if (!msg) return;
+  if (!i) return;
   let user = i.members.cache.find(
     m =>
       m.user.username.toLowerCase().startsWith(msg) ||
@@ -43,7 +56,7 @@ function match(msg, i) {
       m.displayName.toLowerCase() === msg ||
       m.displayName.toLowerCase().includes(msg)
   );
-  if (!user) return undefined;
+  if (!user) return;
   return user.user;
 }
 
