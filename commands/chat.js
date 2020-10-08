a836b8be3c92d78220f5372679d9e4c518ba841b
Snowflake <devsnowflake107@gmail.com>
2020-10-08 17:48:09 +0545

    add chatbot pt 2

--- commands/chat.js
+++ commands/chat.js
@@ -3,10 +3,19 @@ const fetch = require("node-fetch");
 module.exports.run = (client, message, args) => {
     let mesg = args.join(" ");
     if (!mesg) return message.channel.send("Please say something.");
-    fetch(`http://api.brainshop.ai/get?bid={YOUR_BRAIN_ID}&key={YOUR_API_KEY}&uid=1&msg=${encodeURIComponent(mesg)}`)
+    
+    // part one
+    // fetch(`http://api.brainshop.ai/get?bid={YOUR_BRAIN_ID}&key={YOUR_API_KEY}&uid=1&msg=${encodeURIComponent(mesg)}`)
+    // .then(res => res.json())
+    // .then(data => {
+    // message.channel.send(data.cnt);
+    // });
+    
+    // part two
+    fetch(`https://some-random-api.ml/chatbot?message=${encodeURIComponent(mesg)}`)
     .then(res => res.json())
     .then(data => {
-        message.channel.send(data.cnt);
+        message.channel.send(data.response);
     });
 }
 
