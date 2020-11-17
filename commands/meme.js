5482c94b4d28ac34df879e30d9007a9c94344c9b
udit2303 <69803864+udit2303@users.noreply.github.com>
2020-11-17 16:21:38 +0530

    Create meme.js

---
+++ commands/meme.js
@@ -0,0 +1,24 @@
+const fetch = require('node-fetch');
+module.exports.run = (client, message, args) {
+fetch(`https://api.snowflakedev.xyz/meme`)
+.then(response => {
+        response.json()
+        .then(json => {
+        const meme = {
+          title: json.title,
+          img: json.url,
+          link: json.link,
+          subreddit: json.subreddit
+        }
+        const memeembed = new Discord.MessageEmbed()
+        .setTitle(`${meme.title}`)
+        .setImage(meme.img)
+        .setColor('RANDOM')
+        .setURL(meme.link);
+        return message.channel.send(memeembed).catch(console.error);
+        });
+});
+}
+module.exports.help = {
+name:"meme"
+}
