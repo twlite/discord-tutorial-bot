05b9ed8f5dfe086c5283b90b030552047e0f11dd
Snowflake <devsnowflake107@gmail.com>
2020-11-21 11:51:49 +0545

    reformat code

--- commands/meme.js
+++ commands/meme.js
@@ -1,24 +1,30 @@
 const fetch = require('node-fetch');
+
 module.exports.run = (client, message, args) => {
-fetch(`https://api.snowflakedev.xyz/meme`)
-.then(response => {
-        response.json()
-        .then(json => {
-        const meme = {
-          title: json.title,
-          img: json.url,
-          link: json.link,
-          subreddit: json.subreddit
-        }
-        const memeembed = new Discord.MessageEmbed()
-        .setTitle(`${meme.title}`)
-        .setImage(meme.img)
-        .setColor('RANDOM')
-        .setURL(meme.link);
-        return message.channel.send(memeembed).catch(console.error);
-        });
-});
+	fetch(`https://api.snowflakedev.xyz/meme`)
+		.then(response => response.json())
+		.then(json => {
+			const meme = {
+				title: json.title,
+				img: json.url,
+				link: json.link,
+				subreddit: json.subreddit
+			}
+
+			const memeembed = new Discord.MessageEmbed()
+				.setTitle(`${meme.title}`)
+				.setImage(meme.img)
+				.setColor('RANDOM')
+				.setURL(meme.link);
+
+			return message.channel.send(memeembed)
+		})
+		.catch(e => {
+			console.error(e);
+			message.channel.send("something went wrong!");
+		});
 }
+
 module.exports.help = {
-name:"meme"
+	name:"meme"
 }
