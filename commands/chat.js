a31f6d60097d7d20a40daf961e8579d2fb9ecf48
udit2303 <69803864+udit2303@users.noreply.github.com>
2020-12-10 11:52:18 +0530

    Update chat.js

--- commands/chat.js
+++ commands/chat.js
@@ -1,28 +1,10 @@
-const fetch = require("node-fetch");
-
-module.exports.run = (client, message, args) => {
+module.exports.run = (client, message, args, api) => {
     let mesg = args.join(" ");
     if (!mesg) return message.channel.send("Please say something.");
-    
-    // part one
-    // fetch(`http://api.brainshop.ai/get?bid={YOUR_BRAIN_ID}&key={YOUR_API_KEY}&uid=1&msg=${encodeURIComponent(mesg)}`)
-    // .then(res => res.json())
-    // .then(data => {
-    // message.channel.send(data.cnt);
-    // });
-    
-    // part two
-    /*fetch(`https://some-random-api.ml/chatbot?message=${encodeURIComponent(mesg)}`)
-    .then(res => res.json())
-    .then(data => {
-        message.channel.send(data.response);
-    });*/
-    // part three
-    fetch (`https://api.snowflakedev.xyz/chatbot?message=${encodeURIComponent(message.content)}&name=${encodeURIComponent(client.user.username)}`)
-        .then(data => {
-        const json = data.json();
-        message.channel.send(json.message);
-    });
+   
+    api.chatbot(mesg).then(msg => {
+        message.channel.send(msg);
+    })
 }
 
 module.exports.help = {
