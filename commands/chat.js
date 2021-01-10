480e7917f4e5e4a444f73866043e9d3f8c953aba
Snowflake <devsnowflake107@gmail.com>
2021-01-10 13:47:21 +0545

    Update chat.js

--- commands/chat.js
+++ commands/chat.js
@@ -2,7 +2,7 @@ module.exports.run = (client, message, args, api) => {
     let mesg = args.join(" ");
     if (!mesg) return message.channel.send("Please say something.");
    
-    api.chatbot(mesg).then(msg => {
+    client.snowapi.chatbot({ message: mesg }).then(msg => {
         message.channel.send(msg);
     })
 }
